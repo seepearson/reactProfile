5a577cd92fc90a5b73bfe3531e7b7a52ab2328bf
Cassy Pearson <seepearson@gmail.com>
2020-08-03 10:05:58 -0700

    styling added

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,10 @@ import Box from '@material-ui/core/Box';
 export const Home = () => {
     return (
         <Container maxWidth="lg">
-                <p><strong>Hello!</strong> My name is Cassy Pearson</p>
+              
+                <h1><strong>Hello! Thank you for viewing my portfolio!</strong> </h1>
+               
+                <h2>Front-end web developer with a background in Secondary Education. Recently earned a certificate in Full Stack Development from the University of Arizona with newly developed skills in JavaScript, CSS, and responsive web design. Also has a bachelor's of arts degree in Social Studies teaching from Brigham Young University. Known for the ability to build relationships with others and being an effective team leader. Has patience and resilience when working on a project. Is focused on bringing creativity and design to lackluster projects. Will continue learning and growing in web development. Excited to encounter challenges and new experiences in the future as part of a fast-paced, quality-driven team in order to build better experiences on the web. </h2>
         </Container>
     )
 }
