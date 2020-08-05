4c91f10bd163b48ee3a13d5d696ae27e73bb267c
Cassy Pearson <seepearson@gmail.com>
2020-08-05 14:14:12 -0700

    netlify

---
+++ build/precache-manifest.c600bdee1eeb1bb416cd589236c66a9a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b7a71ca0a283494598ac6f300d8569",
+    "url": "/e60b15daa51dd5ca873c.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/e60b15daa51dd5ca873c.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d22ca68c8a89fa465423c00cb01ecec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2486d2db083815cf3ab5",
+    "url": "/static/css/2.af0132db.chunk.css"
+  },
+  {
+    "revision": "cdf0a9b2c9314a6884c0",
+    "url": "/static/css/main.189ade29.chunk.css"
+  },
+  {
+    "revision": "2486d2db083815cf3ab5",
+    "url": "/static/js/2.9ba724b9.chunk.js"
+  },
+  {
+    "revision": "a74d03541b31c9edb8e975400075ea2b",
+    "url": "/static/js/2.9ba724b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf0a9b2c9314a6884c0",
+    "url": "/static/js/main.61ed9f45.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
