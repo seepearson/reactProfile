f91d746de7b384ca4dee1eb7e4cc370000ae4c57
Cassy Pearson <seepearson@gmail.com>
2020-08-05 14:56:21 -0700

    removed express

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Express from 'express';
+
 
 ReactDOM.render(
   <React.StrictMode>
