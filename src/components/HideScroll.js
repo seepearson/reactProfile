53c4b19c8802df7301d70cbbf01f52e35528405c
Cassy Pearson <seepearson@gmail.com>
2020-08-05 18:32:53 -0700

    trying to fix material-ui

--- src/components/HideScroll.js
+++ src/components/HideScroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
+import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 // import Box from '@material-ui/core/Box';
