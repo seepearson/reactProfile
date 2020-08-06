2a3a9f631186575cb67da277f56208b2890b3d71
Cassy Pearson <seepearson@gmail.com>
2020-08-05 19:13:14 -0700

    took out material ui things

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+//import Box from '@material-ui/core/Box';
 // import Paper from '@material-ui/core/Paper';
 // import { sizing } from '@material-ui/system';
 
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,7 +4,7 @@ import GridList from '@material-ui/core/GridList';
 import Link from '@material-ui/core/Link';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
+//import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import Typography from '@material-ui/core/typography'
